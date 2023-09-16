1e270f29235a0fc355041ef5859cfebb691088aa
Trachuk Maksym <tracukmaksim@gmail.com>
2023-09-17 00:59:58 +0300

    Change models and create db conn

--- models/models.py
+++ models/models.py
@@ -1,29 +1,21 @@
 from datetime import datetime
 from uuid import uuid4
 
-from sqlalchemy import (
-    UUID,
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    create_engine,
-)
+from sqlalchemy import (UUID, Column, DateTime, ForeignKey, Integer, String,
+                        create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-engine = create_engine("postgresql://maksym:123456@localhost/graduation_db")
-Base = declarative_base()
+from .db import Base
 
 
-class Client(Base):
-    __tablename__ = "Clients"
+class Clients(Base):
+    __tablename__ = "clients"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     username = Column(String(50), nullable=False)
     email = Column(String(255), nullable=False)
-    password = Column(String(36), nullable=False)
+    password = Column(String(255), nullable=False)
     phone = Column(String(20), nullable=False)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
@@ -32,8 +24,8 @@ class Client(Base):
     clientpurchase = relationship("ClientsPurchases", backref="client")
 
 
-class Meeting(Base):
-    __tablename__ = "Meetings"
+class Meetings(Base):
+    __tablename__ = "meetings"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     place = Column(String(100), nullable=False)
@@ -42,39 +34,41 @@ class Meeting(Base):
     update_date = Column(DateTime, default=datetime.utcnow)
     clientmeeting = relationship("ClientsMeetings", backref="meeting")
     meetingcomment = relationship("MeetingsComments", backref="meeting")
+    purchases = relationship("Purchases", backref="meeting")
 
 
-class ClientMeeting(Base):
-    __tablename__ = "ClientsMeetings"
+class ClientsMeetings(Base):
+    __tablename__ = "clientsmeetings"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    client_id = Column(UUID(as_uuid=True), ForeignKey("Clients.id"), default=uuid4)
-    meeting_id = Column(UUID(as_uuid=True), ForeignKey("Meetings.id"), default=uuid4)
+    client_id = Column(UUID(as_uuid=True), ForeignKey("clients.id"), default=uuid4)
+    meeting_id = Column(UUID(as_uuid=True), ForeignKey("meetings.id"), default=uuid4)
 
 
-class Comment(Base):
-    __tablename__ = "Comments"
+class Comments(Base):
+    __tablename__ = "comments"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     comment = Column(String)
-    client_id = Column(UUID(as_uuid=True), ForeignKey("Clients.id"), default=uuid4)
+    client_id = Column(UUID(as_uuid=True), ForeignKey("clients.id"), default=uuid4)
     create_date = Column(DateTime, default=datetime.utcnow)
     update_date = Column(DateTime, default=datetime.utcnow)
     meetingcomment = relationship("MeetingsComments", backref="comment")
 
 
-class MeetingComment(Base):
-    __tablename__ = "MeetingsComments"
+class MeetingsComments(Base):
+    __tablename__ = "meetingscomments"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    comment_id = Column(UUID(as_uuid=True), ForeignKey("Comments.id"), default=uuid4)
-    meeting_id = Column(UUID(as_uuid=True), ForeignKey("Meetings.id"), default=uuid4)
+    comment_id = Column(UUID(as_uuid=True), ForeignKey("comments.id"), default=uuid4)
+    meeting_id = Column(UUID(as_uuid=True), ForeignKey("meetings.id"), default=uuid4)
 
 
-class Purchase(Base):
-    __tablename__ = "Purchases"
+class Purchases(Base):
+    __tablename__ = "purchases"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    meeting_id = Column(UUID(as_uuid=True), ForeignKey("meetings.id"), default=uuid4)
     purchase_amount = Column(Integer, nullable=False)
     description = Column(String(30), nullable=False)
     create_date = Column(DateTime, default=datetime.utcnow)
@@ -82,12 +76,9 @@ class Purchase(Base):
     clientpurchase = relationship("ClientsPurchases", backref="purchase")
 
 
-class ClientPurchase(Base):
-    __tablename__ = "ClientsPurchases"
+class ClientsPurchases(Base):
+    __tablename__ = "clientspurchases"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    client_id = Column(UUID(as_uuid=True), ForeignKey("Clients.id"), default=uuid4)
-    purchase_id = Column(UUID(as_uuid=True), ForeignKey("Purchases.id"), default=uuid4)
-
-
-Base.metadata.create_all(engine)
+    client_id = Column(UUID(as_uuid=True), ForeignKey("clients.id"), default=uuid4)
+    purchases_id = Column(UUID(as_uuid=True), ForeignKey("purchases.id"), default=uuid4)
